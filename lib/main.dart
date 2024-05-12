fda39a88209ae800377fdbf168cd1d58d42b9f42
unknown <yes.kanat@gmail.com>
2024-05-13 00:27:07 +0300

    project started

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tips_app/presentations/pages/start_screen.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: StartScreen(),
+  ));
+}
