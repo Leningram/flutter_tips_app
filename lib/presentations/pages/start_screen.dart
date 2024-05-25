257e7ff0aca2b39dc27b6653501ef5a4bd1f0bc6
Kanat <k.esenamanov@smfox.ri>
2024-05-25 22:25:19 +0300

    styles

--- lib/presentations/pages/start_screen.dart
+++ lib/presentations/pages/start_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_tips_app/data/models/employee.dart';
 import 'package:flutter_tips_app/data/utils/formatters.dart';
@@ -14,57 +16,72 @@ class StartScreen extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Card(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
           margin: const EdgeInsets.fromLTRB(20, 20, 20, 0),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  'Дениска',
-                  style: bodyText2,
-                ),
-                const SizedBox(
-                  height: 5,
-                ),
-                Text(
-                  formatNumber(16720),
-                  style: titleText1,
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                const Row(
-                  children: [
-                    Text(
-                      'часов:',
-                      style: bodyText1,
-                    ),
-                    SizedBox(
-                      width: 5,
-                    ),
-                    Text(
-                      '45',
-                      style: bodyText1,
-                    ),
-                  ],
-                ),
-                const Row(
-                  children: [
-                    Text(
-                      'аванс:',
-                      style: bodyText1,
-                    ),
-                    SizedBox(
-                      width: 5,
-                    ),
-                    Text(
-                      '12000',
-                      style: bodyText1,
-                    ),
-                  ],
-                ),
-              ],
+          child: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Theme.of(context).colorScheme.primary,
+                  Theme.of(context).colorScheme.primary,
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Дениска',
+                    style: bodyText2,
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    formatNumber(16720),
+                    style: titleText1,
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Row(
+                    children: [
+                      Text(
+                        'часов:',
+                        style: bodyText1,
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        '45',
+                        style: bodyText1,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      const Text(
+                        'аванс:',
+                        style: bodyText1,
+                      ),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        formatNumber(12000),
+                        style: bodyText1,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
@@ -77,10 +94,11 @@ class StartScreen extends StatelessWidget {
         ),
         Expanded(
           child: ListView.builder(
-            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 0),
             itemCount: employees.length,
             itemBuilder: (ctx, index) => Container(
-                color: index % 2 == 0 ? Color.fromARGB(30, 151, 151, 151) : null,
+                color: index % 2 == 0
+                    ? Theme.of(context).colorScheme.primaryContainer
+                    : null,
                 child: EmployeeItem(employeeData: employees[index])),
           ),
         ),
--- lib/styles/text.styles.dart
+++ lib/styles/text.styles.dart
@@ -3,16 +3,19 @@ import 'package:flutter/material.dart';
 const TextStyle bodyText1 = TextStyle(
   fontSize: 18,
   fontWeight: FontWeight.w500,
+  color: Colors.white,
   height: 1.2,
 );
 
 const TextStyle bodyText2 = TextStyle(
   fontSize: 18,
-  fontWeight: FontWeight.bold,
+  fontWeight: FontWeight.w500,
+  color: Colors.white,
 );
 
 const TextStyle titleText1 = TextStyle(
   fontSize: 34,
   fontWeight: FontWeight.bold,
   height: 1.0,
+  color: Colors.white,
 );
